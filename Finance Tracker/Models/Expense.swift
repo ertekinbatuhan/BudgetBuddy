d8781419846fa9253878c117a43bce7ab65d4e9b
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-08-03 11:18:46 +0300

    update Models

--- Finance Tracker/Models/Expense.swift
+++ Finance Tracker/Models/Expense.swift
@@ -11,17 +11,23 @@ import SwiftData
 @Model
 class Expense {
     
-    var title : String
-    var subTitle : String
-    var amount : Double
-    var date : Date
-    var category : Category?
+    var title: String
+    var subTitle: String
+    var amount: Double
+    var date: Date
+    var category: Category?
+    var type: String
     
-    init(title: String, subTitle: String, amount: Double, date: Date, category: Category? = nil) {
+    init(title: String, subTitle: String, amount: Double, date: Date, type: FinanceType, category: Category? = nil) {
         self.title = title
         self.subTitle = subTitle
         self.amount = amount
         self.date = date
+        self.type = type.rawValue
         self.category = category
+    }
+    
+    var expenseType: FinanceType{
+        return FinanceType(rawValue: type) ?? .expense
     }
 }
