e18030e3e31f3abd2b213a2b9ec5492e60c3eeaf
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-08-13 21:19:46 +0300

    add NotificationManager to Services

---
+++ Finance Tracker/Services/NotificationManager.swift
@@ -0,0 +1,41 @@
+//
+//  NotificationManager.swift
+//  Finance Tracker
+//
+//  Created by Batuhan Berk Ertekin on 13.08.2024.
+//
+
+import Foundation
+import UserNotifications
+
+class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
+    
+    static let shared = NotificationManager()
+    
+    private override init() {
+        super.init()
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func sendNotification(content: UNMutableNotificationContent, trigger: UNNotificationTrigger) {
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("Notification scheduled successfully")
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+}
