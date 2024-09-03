b84b9d2b52bbaf513c8c0645af6a869cb4d532bf
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-09-03 22:33:46 +0300

    add Constants

---
+++ Finance Tracker/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Finance Tracker
+//
+//  Created by Batuhan Berk Ertekin on 1.09.2024.
+//
+
+import Foundation
+
+struct Constants {
+    struct URLs {
+        static let coinURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h&locale=en"
+    }
+}
