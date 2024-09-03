e98722d34a56556515ff07262dd62258c3648616
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-09-03 22:34:24 +0300

    add CoinService

---
+++ Finance Tracker/Services/CoinService.swift
@@ -0,0 +1,48 @@
+//
+//  CoinService.swift
+//  Finance Tracker
+//
+//  Created by Batuhan Berk Ertekin on 1.09.2024.
+//
+
+import Foundation
+import Alamofire
+
+//MARK: - Protocol
+protocol CoinServiceProtocol {
+    func fetchCoins(completion: @escaping (Result<[Coin], CoinError>) -> Void)
+}
+
+// MARK: - CoinService
+// Implements CoinServiceProtocol to fetch coin data.
+class CoinService : CoinServiceProtocol{
+    
+    // MARK: - fetchCoins
+    // Fetches coin data from the API and handles the response.
+    func fetchCoins(completion: @escaping (Result<[Coin], CoinError>) -> Void) {
+        let url = Constants.URLs.coinURL
+        
+        AF.request(url, method: .get).responseDecodable(of: [Coin].self) { response in
+            switch response.result {
+            case .success(let coins):
+                completion(.success(coins))
+                
+            case .failure(let error):
+                let coinError: CoinError
+                
+                if let afError = error.asAFError {
+                    switch afError {
+                    case .responseValidationFailed(reason: .dataFileReadFailed):
+                        coinError = .invalidResponse
+                    default:
+                        coinError = .networkError(error.localizedDescription)
+                    }
+                } else {
+                    coinError = .decodingError
+                }
+                
+                completion(.failure(coinError))
+            }
+        }
+    }
+}
