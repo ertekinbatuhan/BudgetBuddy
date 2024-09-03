c4c7edc018303230a2d03c75f0c228fb5769a9d1
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-09-03 22:44:15 +0300

    add SectionHeader to Components

---
+++ Finance Tracker/Components/SectionHeader.swift
@@ -0,0 +1,28 @@
+//
+//  SectionHeader.swift
+//  Finance Tracker
+//
+//  Created by Batuhan Berk Ertekin on 3.09.2024.
+//
+
+import SwiftUI
+
+// MARK: - SectionHeader
+// A view that displays a section header with a title.
+struct SectionHeader: View {
+    // MARK: - Properties
+    let title: String
+    
+    // MARK: - Environment
+    @Environment(\.colorScheme) var colorScheme
+    
+    // MARK: - Body
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.bold)
+            .padding(.horizontal)
+            .foregroundColor(colorScheme == .dark ? .white : .black)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
