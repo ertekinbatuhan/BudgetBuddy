a0ccfa9d7928f2e7fd77ab095591865f636fdbab
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2024-09-19 21:13:13 +0300

    updated SummaryView to use FinanceViewModel for calculating total amounts.

--- Finance Tracker/Views/SummaryView.swift
+++ Finance Tracker/Views/SummaryView.swift
@@ -11,18 +11,22 @@ struct SummaryView: View {
     // MARK: - Environment Variables
     @Environment(\.modelContext) private var context
     @Environment(\.colorScheme) private var colorScheme
+    // MARK: - View Models
+    @ObservedObject private var viewModel = FinanceViewModel()
+    // MARK: - State Variables
+    @State private var isShowingCalendar = false
     
     var body: some View {
         NavigationStack {
             VStack(spacing: 16) {
-                InfoCardView(title: "GENERAL_TOTAL", amount: totalAmount(for: .income) + totalAmount(for: .expense), color: .blue)
+                InfoCardView(title: "GENERAL_TOTAL", amount: viewModel.totalAmount(for: .income) + viewModel.totalAmount(for: .expense), color: .blue)
                     .padding()
                 
                 HStack(spacing: 16) {
-                    InfoCardView(title: "GENERAL_INCOME", amount: totalAmount(for: .income), color: .green)
+                    InfoCardView(title: "GENERAL_INCOME", amount: viewModel.totalAmount(for: .income), color: .green)
                         .frame(maxWidth: .infinity)
                     
-                    InfoCardView(title: "GENERAL_EXPENSES", amount: totalAmount(for: .expense), color: .red)
+                    InfoCardView(title: "GENERAL_EXPENSES", amount: viewModel.totalAmount(for: .expense), color: .red)
                         .frame(maxWidth: .infinity)
                 }
                 .padding()
@@ -35,13 +39,20 @@ struct SummaryView: View {
             .navigationTitle("FINANCE_STATUS")
             .background(colorScheme == .dark ? Color.black : Color.white)
             .padding()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        isShowingCalendar.toggle()
+                    }) {
+                        Image(systemName: "calendar")
+                        
+                    }
+                }
+            }
+            .sheet(isPresented: $isShowingCalendar) {
+                CalendarView()
+            }
         }
-    }
-
-    func totalAmount(for type: FinanceType) -> Double {
-        return allFinances
-            .filter { $0.financeType == type }
-            .reduce(0) { $0 + $1.amount }
     }
 }
 
